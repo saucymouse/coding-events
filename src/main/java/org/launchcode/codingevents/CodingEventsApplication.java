7f335c77d757011ecc732bbbaab64d39d903d500
Will Staton <will.staton@gmail.com>
2021-06-02 16:47:54 -0400

    finished lesson

--- src/main/java/org/launchcode/codingevents/CodingEventsApplication.java
+++ src/main/java/org/launchcode/codingevents/CodingEventsApplication.java
@@ -1,5 +1,7 @@
 package org.launchcode.codingevents;
 
+import org.launchcode.codingevents.data.EventData;
+import org.launchcode.codingevents.models.Event;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +10,13 @@ public class CodingEventsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CodingEventsApplication.class, args);
+		Event event1 = new Event("Pie Eating Contest", "No forks allowed", "pieman@eatme.com");
+		Event event2 = new Event("Fashion Show for Nerds", "Also known as a convention", "pinky@powerranger.com");
+		Event event3 = new Event("LOSERS CLUB", "NO COOL PPL ALLOWED", "iamlame@dumb.com");
+		EventData.add(event1);
+		EventData.add(event2);
+		EventData.add(event3);
 	}
+
 
 }
