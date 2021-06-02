ff61913d0cad559ade2cfffed49010043d23bf50
Will Staton <will.staton@gmail.com>
2021-06-02 12:59:39 -0400

    finished and moving on to ch13

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -2,6 +2,9 @@ package org.launchcode.codingevents.models;
 
 import java.util.Objects;
 
+/**
+ * Created by Chris Bay
+ */
 public class Event {
 
     private int id;
@@ -17,20 +20,20 @@ public class Event {
         nextId++;
     }
 
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-    //TODO mess with changing method name for getters
     public String getName() {
         return name;
     }
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 
     public int getId() {
